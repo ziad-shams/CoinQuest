3f558baac4a936e8d1cc365e878daa81171095ba
ziad-shams <ziadamr2002@outlook.com>
2025-06-15 04:59:44 +0300

    fixed xp gain

--- backend/utils/xpLogic.js
+++ backend/utils/xpLogic.js
@@ -1,12 +1,7 @@
 const LEVEL_THRESHOLDS = [0, 100, 250, 500, 1000, 2000]; // XP needed per level
 
-function getLevelFromXP(xp) {
-  let level = 1;
-  for (let i = 0; i < LEVEL_THRESHOLDS.length; i++) {
-    if (xp >= LEVEL_THRESHOLDS[i]) level = i + 1;
-    else break;
-  }
-  return level;
+function getLevelFromXP(xp) { 
+  return Math.floor(xp / 100) + 1;
 }
 
 module.exports = {
